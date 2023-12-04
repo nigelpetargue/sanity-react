c281c216d713b3faa4f91106385e5e72066dd0dd
Nigel Petargue <nigelpetargue@gmail.com>
2023-12-04 23:50:05 +0800

    add helper for generating image url from sanity

---
+++ src/utils/helper.js
@@ -0,0 +1,8 @@
+import { client } from '../lib/sanity';
+import imageUrlBuilder from '@sanity/image-url';
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source) {
+  return builder.image(source);
+}
