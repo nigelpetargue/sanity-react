0f52e409c7bbbb9e6c9620a0d38d0bd1ddac2943
Nigel Petargue <nigelpetargue@gmail.com>
2023-12-04 23:48:07 +0800

    create GROQ handler

--- src/utils/handlers.js
+++ src/utils/handlers.js
@@ -1,6 +1,23 @@
 import { client } from '../lib/sanity';
 
 export async function getPost() {
-  const posts = await client.fetch(`*[_type == "post"]`);
+  const posts = await client.fetch(`*[_type == 'post'] {
+    _id,
+    title,
+    slug,
+    subheading,
+    readingTime,
+    body,
+    publishedAt,
+    mainImage,
+    author -> {
+      name,
+      bio,
+      image
+    },
+    categories[] -> {
+      title
+    }
+  } | order(publishedAt desc)`);
   return posts;
 }
